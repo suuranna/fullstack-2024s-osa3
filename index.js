d2ff96083215157e8ff5c3b175c403e2e6ea0a41
Anna Suuronen <annasuuronen@icloud.com>
2024-11-30 01:20:36 +0200

    tehtävä 3.5 tehty

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "name": "Arto Hellas",
@@ -49,6 +51,26 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const id = Math.floor(Math.random() * 10000)
+  console.log(request.body)
+  const body = request.body
+
+  if (!body.name | !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id.toString()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
