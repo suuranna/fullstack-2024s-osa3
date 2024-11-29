f0160848a5b7e9997acf2452ff1998f1cfbe8df4
Anna Suuronen <annasuuronen@icloud.com>
2024-11-30 00:43:09 +0200

    tehtävä 3.4 tehty

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const { stringify } = require('querystring')
 const app = express()
 
-const persons = [
+let persons = [
   {
     "name": "Arto Hellas",
     "number": "040-123456",
@@ -43,6 +42,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
