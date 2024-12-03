8da2faecd8b1aab4599ab4b77367a7c6d796fc87
Anna Suuronen <annasuuronen@icloud.com>
2024-12-03 03:28:34 +0200

    tehtävä 3.17 tehty

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.use(morgan(function (tokens, request, response) {
 
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 let persons = [
   {
     "name": "Arto Hellas",
@@ -47,10 +61,11 @@ let persons = [
   }
 ]
 
-const info = '<div><p>Phonebook has info for ' + persons.length + ' people</p><p>' + new Date() + '</p></div>'
-
 app.get('/info', (request, response) => {
-  response.send(info)
+  Person.find({}).then(persons => {
+    const info = '<div><p>Phonebook has info for ' + persons.length.toString() + ' people</p><p>' + new Date() + '</p></div>'
+    response.send(info)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -59,14 +74,18 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  //const person = persons.find(person => person.id === id)
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -105,6 +124,25 @@ app.post('/api/persons', (request, response) => {
   })
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
