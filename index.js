5990bf3e39ff8512d38f218a9c777bc8c0805946
Anna Suuronen <annasuuronen@icloud.com>
2024-12-03 03:52:06 +0200

    tehtävä 3.19 tehty

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
+  
   next(error)
 }
 
@@ -98,18 +100,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
       .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   //const id = Math.floor(Math.random() * 10000)
   console.log(request.body)
   const body = request.body
 
-  if (!body.name | !body.number) {
-    return response.status(400).json({ error: 'name or number missing' })
-  }
+  //if (!body.name | !body.number) {
+  //  return response.status(400).json({ error: 'name or number missing' })
+  //}
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
+  //if (persons.find(person => person.name === body.name)) {
+  //  return response.status(400).json({ error: 'name must be unique' })
+  //}
 
   const person = new Person({
     name: body.name,
@@ -122,6 +124,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(person => {
     response.json(person)
   })
+  .catch(error => next(error))
 
 })
 
